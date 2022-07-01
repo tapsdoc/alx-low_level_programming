4c50646fa16bed7b8dbb31443cdef4b56f2a89c9
Tapiwa Chiremba <chirembatapiwa17@gmail.com>
2022-06-30 19:47:27 -0700

    Added changes to 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,9 @@
 
 /**
  * main - prints the size of various types
- * Retrun: 0 if exited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  */
+
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
