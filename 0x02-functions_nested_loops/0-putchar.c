581420c30dd0fb71b2ef508bc15acfe29197289b
Tapiwa Chiremba <chirembatapiwa17@gmail.com>
2022-07-05 11:26:44 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar('_\n');
+	putchar('p\n');
+	putchar('u\n');
+	putchar('t\n');
+	putchar('c\n');
+	putchar('h\n');
+	putchar('a\n');
+	putchar('r\n');
+
+	return (0);
+}
