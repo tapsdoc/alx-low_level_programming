aebc7ebc1ee6c838fc7212af42501914c16d8d96
Tapiwa Chiremba <chirembatapiwa17@gmail.com>
2022-07-11 11:58:25 -0700

    Added task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, len, len2;
+
+	len = 0;
+	len2 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len2 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len2];
+		s[len2--] = tmp;
+	}
+}
